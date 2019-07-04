797792fee8cdf31cdcfc0b08841af96f93a72ccb
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2019-07-05 00:46:30 +0200

    Add comments

--- src/Kernel/ErrorHandler.php
+++ src/Kernel/ErrorHandler.php
@@ -18,12 +18,25 @@
      */
     final class ErrorHandler
     {
+        /**
+         * Register ErrorHandler into PHP
+         */
         public function register()
         {
             /// TODO: Add ability to catch Fatal Errors (PHP would still exit, but hey)
             set_error_handler([$this, 'throwException']);
         }
 
+        /**
+         * Simple trampoline, that ejects Error into Exceptions
+         *
+         * @param int    $level   Severity of Error
+         * @param string $message Message associated with error
+         * @param string $file    File where error occurred
+         * @param int    $line    Line on which error occurred
+         *
+         * @throws \ErrorException
+         */
         public function throwException(int $level, string $message, string $file, int $line)
         {
             /// TODO: Maybe rethrow NoticeException and stuff
