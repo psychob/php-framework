1d7b98fa53bb946f8e3693a5cdf90e25a10d2151
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2019-07-24 12:33:34 +0200

    Add FrameworkSource

---
+++ src/FrameworkSource.php
@@ -0,0 +1,19 @@
+<?php
+    //
+    // psychob/framework
+    // (c) 2019 RGB Lighthouse <https://rgblighthouse.pl>
+    // (c) 2019 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+    //
+
+    namespace PsychoB\Framework;
+
+    /**
+     * Class used to retrieve __DIR__ of framework, without parsing composer file
+     *
+     * @author Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+     * @since  0.1
+     */
+    class FrameworkSource
+    {
+        public const CURRENT_DIR = __DIR__;
+    }
