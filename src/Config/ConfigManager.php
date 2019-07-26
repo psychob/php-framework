94916eef249edd189463fb8a44bde27e0b05814a
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2019-07-26 11:27:45 +0200

    ConfigManager

---
+++ src/Config/ConfigManager.php
@@ -0,0 +1,91 @@
+<?php
+    //
+    // psychob/framework
+    // (c) 2019 RGB Lighthouse <https://rgblighthouse.pl>
+    // (c) 2019 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+    //
+
+    namespace PsychoB\Framework\Config;
+
+    use PsychoB\Framework\Utility\Arr;
+    use PsychoB\Framework\Utility\Path;
+    use PsychoB\Framework\Utility\Str;
+
+    class ConfigManager implements ConfigManagerInterface
+    {
+        protected $configPaths = [];
+        protected $loaded = [];
+        protected $container = [];
+
+        /**
+         * ConfigManager constructor.
+         *
+         * @param string $frameworkPath
+         * @param string $appPath
+         */
+        public function __construct(string $frameworkPath, string $appPath)
+        {
+            $this->configPaths = [
+                $frameworkPath,
+                $appPath,
+            ];
+        }
+
+        /** @inheritDoc */
+        public function get(string $key, $default)
+        {
+            $components = Str::explode($key, '.');
+
+            if (!$this->hasLoaded($components[0])) {
+                $this->load($components[0]);
+            }
+
+            return Arr::recursiveGet($this->container, $components, $default);
+        }
+
+        private function hasLoaded(string $component): bool
+        {
+            return Arr::has($this->loaded, $component);
+        }
+
+        private function load(string $component): void
+        {
+            $branch = [];
+
+            foreach ($this->configPaths as $path) {
+                $branch[] = $this->loadPath($path, $component);
+            }
+
+            $this->container[$component] = $this->mergeBranches($branch);
+        }
+
+        protected function loadPath(string $path, string $component): array
+        {
+            $path = Path::join($path, $component . '.php');
+            $content = $this->parseFile($path);
+
+            $this->loaded[$component][] = $path;
+
+            return $content;
+        }
+
+        protected function parseFile(string $path): array
+        {
+            if (Path::fileExists($path)) {
+                return __nondirect_include($path);
+            }
+
+            return [];
+        }
+
+        private function mergeBranches(array $branch): array
+        {
+            return Arr::merge(Arr::MERGE_USE_LATEST, ...$branch);
+        }
+    }
+
+    /** @internal */
+    function __nondirect_include(string $path)
+    {
+        return include($path);
+    }
