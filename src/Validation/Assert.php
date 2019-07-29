dac88daafab651c43cc68bb735dcfc066744f054
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2019-07-29 16:37:26 +0200

    Moving assert from utility to validation

---
+++ src/Validation/Assert.php
@@ -0,0 +1,47 @@
+<?php
+    //
+    // psychob/framework
+    // (c) 2019 RGB Lighthouse <https://rgblighthouse.pl>
+    // (c) 2019 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+    //
+
+    namespace PsychoB\Framework\Validation;
+
+    use PsychoB\Framework\Utility\Arr;
+    use PsychoB\Framework\Validation\Asserts\Exception\AssertionNotFoundException;
+    use PsychoB\Framework\Validation\Asserts\IsEqual;
+    use PsychoB\Framework\Validation\Asserts\IsTrue;
+
+    /**
+     * Class Assert
+     *
+     * @author Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+     * @since  0.1
+     *
+     * @method static isEqual($left, $right, string $message = '')
+     * @method static isTrue($expr, string $message = '')
+     * @method static isFalse($expr, string $message = '')
+     * @method static typeRequirements(object $obj, string $class, mixed[] $properties, string $message = '')
+     * @method static exactType(object $obj, string $class, string $message = '')
+     * @method static hasProps(object $obj, mixed[] $properties, string $message = '')
+     */
+    class Assert
+    {
+        protected static $AssertImpl = [
+            'isEqual' => IsEqual::class,
+            'isTrue' => IsTrue::class,
+            'isFalse' => IsFalse::class,
+            'typeRequirements' => TypeRequirements::class,
+            'exactType' => ExactType::class,
+            'hasProps' => HasProp::class,
+        ];
+
+        public static function __callStatic($name, $arguments)
+        {
+            if (Arr::has(self::$AssertImpl, $name)) {
+                call_user_func_array([self::$AssertImpl[$name], 'ensure'], $arguments);
+            } else {
+                throw new AssertionNotFoundException(Arr::keys(self::$AssertImpl), $name);
+            }
+        }
+    }
