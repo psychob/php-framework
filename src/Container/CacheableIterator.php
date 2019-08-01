b01aa90f65edbed9cc29a5419d5d02b6835ccdc1
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2019-08-01 14:39:57 +0200

    Simple CacheableIterator

---
+++ src/Container/CacheableIterator.php
@@ -0,0 +1,146 @@
+<?php
+    //
+    // psychob/framework
+    // (c) 2019 RGB Lighthouse <https://rgblighthouse.pl>
+    // (c) 2019 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+    //
+
+    namespace PsychoB\Framework\Container;
+
+    use Iterator;
+    use PsychoB\Framework\Assert\Assert;
+    use PsychoB\Framework\Utility\Arr;
+
+    class CacheableIterator implements Iterator
+    {
+        private const STATE_EMPTY    = 0;
+        private const STATE_RUNNING  = 1;
+        private const STATE_PAUSED   = 2;
+        private const STATE_FINISHED = 3;
+
+        /** @var Iterator */
+        private $internalIterator = NULL;
+
+        /** @var mixed */
+        private $container = [];
+
+        /** @var integer */
+        private $insideIterator = 0;
+
+        /** @var integer */
+        private $outsideIterator = 0;
+
+        /** @var int */
+        private $state = self::STATE_EMPTY;
+
+        /**
+         * CacheableIterator constructor. It assumes that $iterator is currently at position 0
+         *
+         * @param Iterator $iterator
+         */
+        public function __construct(Iterator $iterator)
+        {
+            $this->internalIterator = $iterator;
+        }
+
+        /** @inheritDoc */
+        public function current()
+        {
+            switch ($this->state) {
+                case self::STATE_RUNNING:
+                    return $this->internalIterator->current();
+
+                case self::STATE_PAUSED:
+                case self::STATE_FINISHED:
+                    return $this->container[$this->outsideIterator][1];
+
+                default:
+                    Assert::unreachable('Iterator state not supported in current');
+            }
+        }
+
+        /** @inheritDoc */
+        public function next()
+        {
+            switch ($this->state) {
+                case self::STATE_RUNNING:
+                    $this->insideIterator++;
+                    $this->internalIterator->next();
+                    break;
+
+                case self::STATE_FINISHED:
+                case self::STATE_PAUSED:
+                    $this->outsideIterator++;
+                    break;
+
+                default:
+                    Assert::unreachable('Iterator state not supported in next');
+            }
+        }
+
+        /** @inheritDoc */
+        public function key()
+        {
+            switch ($this->state) {
+                case self::STATE_RUNNING:
+                    return $this->internalIterator->key();
+
+                case self::STATE_FINISHED:
+                case self::STATE_PAUSED:
+                    return $this->container[$this->outsideIterator][0];
+
+                default:
+                    Assert::unreachable('Iterator state not supported in key');
+            }
+        }
+
+        /** @inheritDoc */
+        public function valid()
+        {
+            $isValid = $this->internalIterator->valid();
+
+            switch ($this->state) {
+                case self::STATE_RUNNING:
+                    if (!$isValid) {
+                        // we finished running, we can go home now
+                        $this->state = self::STATE_FINISHED;
+                    } else {
+                        $this->container[$this->insideIterator] = [
+                            $this->internalIterator->key(),
+                            $this->internalIterator->current(),
+                        ];
+                    }
+
+                    return $isValid;
+
+                case self::STATE_FINISHED:
+                    return Arr::has($this->container, $this->outsideIterator);
+
+                default:
+                    Assert::unreachable('Iterator state not supported in valid');
+            }
+        }
+
+        /** @inheritDoc */
+        public function rewind()
+        {
+            switch ($this->state) {
+                case self::STATE_EMPTY:
+                    $this->internalIterator->rewind();
+                    $this->state = self::STATE_RUNNING;
+                    break;
+
+                case self::STATE_FINISHED:
+                    $this->outsideIterator = 0;
+                    break;
+
+                case self::STATE_RUNNING:
+                    $this->outsideIterator = 0;
+                    $this->state = self::STATE_PAUSED;
+                    break;
+
+                default:
+                    Assert::unreachable('Iterator state not supported in rewind');
+            }
+        }
+    }
---
+++ tests/Unit/Container/CacheableIteratorTest.php
@@ -0,0 +1,50 @@
+<?php
+    //
+    // psychob/framework
+    // (c) 2019 RGB Lighthouse <https://rgblighthouse.pl>
+    // (c) 2019 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+    //
+
+    namespace Tests\PsychoB\Framework\Unit\Container;
+
+    use PsychoB\Framework\Container\CacheableIterator;
+    use PsychoB\Framework\Testing\UnitTestCase;
+
+    class CacheableIteratorTest extends UnitTestCase
+    {
+        public function testEmptyIterator()
+        {
+            $iterator = new CacheableIterator((function () {
+                return;
+
+                // we are forcing php to return Generator from this function, but because it's behind return, we will
+                // never return an item from it
+                yield;
+            })());
+
+            $this->assertArrayIsEmpty($iterator);
+            $this->assertArrayIsEmpty($iterator);
+            $this->assertArrayIsEmpty($iterator);
+        }
+
+        public function testMultipleItemsIterator()
+        {
+            $iterator = new CacheableIterator((function () {
+                yield 1;
+                yield 2;
+                yield 3;
+            })());
+
+            $this->assertArrayElementsAre([1, 2, 3], $iterator, function ($e, $c, $ek) {
+                $this->assertSame($e, $c, 'Bad element at: ' . $ek);
+            });
+
+            $this->assertArrayElementsAre([1, 2, 3], $iterator, function ($e, $c, $ek) {
+                $this->assertSame($e, $c, 'Bad element at: ' . $ek);
+            });
+
+            $this->assertArrayElementsAre([1, 2, 3], $iterator, function ($e, $c, $ek) {
+                $this->assertSame($e, $c, 'Bad element at: ' . $ek);
+            });
+        }
+    }
